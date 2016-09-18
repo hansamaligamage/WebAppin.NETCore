5298ace9d8d1b8f4e45280fce69b4968ee0e39fd
hansamali <hansamali.gamage@gmail.com>
2016-09-19 00:44:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World - web");
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .Configure(app => 
+                {app.Run(c => c.Response.WriteAsync("Hello"));
+                })
+            .Build();
+            host.Run();
+        }
+    }
+}
